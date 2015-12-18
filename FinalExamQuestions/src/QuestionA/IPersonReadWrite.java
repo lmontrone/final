81075cb0591fc11853f4b33abf342ad6b8f5bfed
Logan Montrone <logan@wifi-roaming-128-4-100-76.host.udel.edu>
2015-12-18 16:02:23 -0500

    committed and pushed

---
+++ FinalExamQuestions/src/QuestionA/IPersonReadWrite.java
@@ -0,0 +1,15 @@
+package QuestionA;
+
+import java.util.Date;
+
+public interface IPersonReadWrite {
+
+	public String setFirstName();
+	public String setMiddleName();
+	public String setLastName();
+	public Date setDOB();
+	public String setAddress();
+	public String setPhone();
+	public String setEmail();
+	
+}
